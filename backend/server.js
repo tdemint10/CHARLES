1b0c2a5c95237c1232126cb7c50f2e3f9d8aeeef
Travis DeMint <trav@Orion.local>
2019-03-25 21:06:12 -0500

    removing connection to mongo

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 /* MONGO CONNECTION */
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
-const connection = mongoose.connection;
-
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-});
+// mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+// const connection = mongoose.connection;
+//
+// connection.once('open', function() {
+//     console.log("MongoDB database connection established successfully");
+// });
 
 /* ROUTES */
 app.get('/', (req, res) => {
