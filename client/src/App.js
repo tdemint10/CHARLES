a0f5503a781a9de73e57c6b1c70367c8b3dc417e
Travis DeMint <trav@Orion.local>
2019-03-26 20:05:24 -0500

    working on front end for todo functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
 
 class App extends Component {
   render() {
     return (
-      <h1>CHARLES</h1>
+      <Router>
+        <div class="container">
+          <h1>CHARLES</h1>
+          <Link to="/">List Todos</Link><br/>
+          <Link to="/create">Create Todo</Link><br/>
+          <Link to="/edit/:id">Edit Todo</Link><br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/edit-todo.component.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class EditTodo extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/todos/' + this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          todo_description: response.data.todo_description,
+          todo_responsible: response.data.todo_responsible,
+          todo_priority: response.data.todo_priority,
+          todo_completed: response.data.todo_completed
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Welcome to Edit Todo Component!!</p>
+      </div>
+    )
+  }
+
+}
