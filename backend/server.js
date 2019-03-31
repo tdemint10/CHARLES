527fdc67fde363ac189d122e73c0526017ea9a05
Travis DeMint <trav@Orion.local>
2019-03-31 14:33:57 -0500

    todo create and list working with mongo locally

--- backend/server.js
+++ backend/server.js
@@ -4,22 +4,55 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+let Todo = require('./models/todo.model');
 
 /* MIDDLEWARE */
 app.use(cors());
 app.use(bodyParser.json());
 
 /* MONGO CONNECTION */
-// mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
-// const connection = mongoose.connection;
-//
-// connection.once('open', function() {
-//     console.log("MongoDB database connection established successfully");
-// });
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
 
-/* ROUTES */
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+});
+
+const todoRoutes = express.Router();
+app.use('/todos', todoRoutes);
+
+/* MAIN ROUTES */
 app.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
+});
+
+/* TODO ROUTES */
+todoRoutes.route('/').get((req, res) => {
+  Todo.find((err, todos) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route('/:id').get((req, res) => {
+  let id = req.params.id;
+  Todo.findById(id, (err, todo) => {
+    res.json(todo);
+  });
+});
+
+todoRoutes.route('/add').post((req, res) => {
+  let todo = new Todo(req.body);
+  todo.save()
+    .then(todo => {
+      res.status(200).json({'todo': 'todo added successfully'});
+    })
+    .catch(err => {
+      res.status(400).send('adding new todo failed');
+    });
 });
 
 /* SET PORT */
--- client/src/components/create-todo.component.js
+++ client/src/components/create-todo.component.js
@@ -52,7 +52,7 @@ export default class CreateTodo extends Component {
       todo_completed: this.state.todo_completed
     };
 
-    axios.post('http://localhost:4000/todos/add', newTodo)
+    axios.post('http://localhost:3001/todos/add', newTodo)
         .then(res => console.log(res.data));
 
     this.setState({
